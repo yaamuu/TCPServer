5219104ea0fe0f9c5365ddc10dab4141e7ff7ef4
Marcin Sieradzki <32650308+yaamuu@users.noreply.github.com>
2018-09-27 10:10:20 +0200

    Initial commit

---
+++ TCPServer/ClientAction.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using DLLGramtoOunces;
+
+namespace TCPServer
+{
+    class ClientAction
+    {
+        public static void DoIt(TcpClient client)
+        {
+            Class1 conventer = new Class1();
+            NetworkStream stream = client.GetStream();
+
+            StreamReader reader = new StreamReader(stream);
+
+            StreamWriter writer = new StreamWriter(stream) {AutoFlush = true};
+
+           
+            while (true)
+            {
+                  
+
+               string request = reader.ReadLine();
+                string[] words = request.Split(' ');
+
+                switch (words[0])
+                {
+                    case "togram":
+
+                        double number = double.Parse(words[1]);
+                        number = conventer.ConvertToGrams(number);
+                        writer.WriteLine(number);
+                        writer.Flush();
+                        break;
+
+                    case "toounces":
+                       
+                        var number1 = double.Parse(words[1]);
+                        number1 = conventer.ConvertToOunces(number1);
+                        writer.WriteLine(number1);
+                        writer.Flush();
+                        break;
+
+                }
+
+
+
+
+            }
+        }
+
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServer
+{
+    class Program
+    {
+        private static readonly int PORT = 8080;
+        static void Main(string[] args)
+        {
+            IPAddress localAddress = IPAddress.Loopback;
+            // ip and port of server
+            TcpListener serverSocket = new TcpListener(localAddress, PORT);
+            //starting server
+            serverSocket.Start();
+            Console.WriteLine("TCP Server running on port " + PORT);
+            while (true) // server loop keeps it running
+            {
+                try
+                {
+                    // waiting for incoming client
+                    TcpClient client = serverSocket.AcceptTcpClient();
+                    Console.WriteLine("incoming client");
+
+                    //allows multiple clients
+                    Task.Run(() => ClientAction.DoIt(client));
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("exception. will continue");
+
+                }
+
+            }
+        }
+    }
+}
